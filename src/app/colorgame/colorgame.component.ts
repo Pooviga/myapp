370986c169ef23686f6646416e2b2ec4fad4a0a9
Github Pooviga <Github spooviga01@gmail.com>
2023-06-27 07:54:07 +0530

    added delete to colorgame

--- src/app/colorgame/colorgame.component.ts
+++ src/app/colorgame/colorgame.component.ts
@@ -3,18 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-colorgame',
   templateUrl: './colorgame.component.html',
-  styleUrls: ['./colorgame.component.css']
+  styleUrls: ['./colorgame.component.css'],
 })
 export class ColorgameComponent {
-  clr="";
+  clr = '';
   // typing(event:any){
   //   console.log(event.target.value)
   //   this.clr=event.target.value;
   // }
-  colors=["pink","orange"];
-  addColor(){
+  colors = ['pink', 'orange'];
+  addColor() {
     this.colors.push(this.clr);
-    this.clr="";
+    this.clr = '';
   }
-
+  deleteItems(i: string) {
+    console.log(i);
+    const index = this.colors.indexOf(i);
+    this.colors.splice(index, 1);
+  }
 }
--- src/app/colour-box/colour-box.component.ts
+++ src/app/colour-box/colour-box.component.ts
@@ -1,10 +1,17 @@
-import { Component , Input} from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { ColorgameComponent } from '../colorgame/colorgame.component';
+// import {colors} from ColorgameComponent;
 @Component({
   selector: 'app-colour-box',
   templateUrl: './colour-box.component.html',
-  styleUrls: ['./colour-box.component.css']
+  styleUrls: ['./colour-box.component.css'],
 })
 export class ColourBoxComponent {
-  @Input() clr : string ="orange";
+  @Input() clr: string = 'orange';
+
+  constructor(private func: ColorgameComponent) {}
+
+  deleteItems(i: string) {
+    this.func.deleteItems(i);
+  }
 }
